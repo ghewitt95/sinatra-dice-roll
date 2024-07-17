e57a2028aeb82b573ec50e8ff80e3bbc4ef91e2b
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2024-07-17 16:15:42 +0000

    made an error somewhere along the way, will redo DOCTYPE template next

--- dice.rb
+++ dice.rb
@@ -2,38 +2,38 @@ require "sinatra"
 require "sinatra/reloader"
 require "better_errors"
 require "binding_of_caller"
+  
 use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
-
-get ("/") do
-erb(:elephant)
+get("/") do
+  erb(:elephant)
 end
 
-get ("/zebra") do
-  "We must add a route for each oath we want to support"
+get("/zebra") do
+  "We must add a route for each path we want to support"
 end
 
-get ("/giraffe") do
-  "Hopefuly this shows up without having to restart the server"
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server"
 end
 
 get("/dice/2/6") do
   first_die = rand(1..6)
-  second_die = rand(1..6)
+	second_die = rand(1..6)
   sum = first_die + second_die
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  erb(:two_six)
+	erb(:two_six)
 end
 
 get("/dice/2/10") do
   first_die = rand(1..10)
-  second_die = rand(1..10)
+	second_die = rand(1..10)
   sum = first_die + second_die
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
   erb(:two_ten)
 end
@@ -41,20 +41,17 @@ end
 get("/dice/1/20") do
   die = rand(1..20)
 
-  @outcome = "You rolled a #{die}."
+	@outcome = "You rolled a #{die}."
 
- erb(:one_twenty)
+	erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
+@@ -58,8 +55,7 @@
   fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
-  
-  @outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die} and a #{fifth_die} for a total of #{sum}"
+  sum = first_die + second_die + third_die + fourth_die
 
- erb(:five_four)
+	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+
+  erb(:five_four)
 end
